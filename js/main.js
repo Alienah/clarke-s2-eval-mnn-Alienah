0c9fe788d497844b6a55fbf3e48e371edc9eb333
Alienah <aidalbla@gmail.com>
2017-12-05 00:54:03 +0100

    Add generator of numbers and results

--- js/main.js
+++ js/main.js
@@ -1,1 +1,46 @@
 'use strict';
+//Generar un número aleatorio entre 1 y 200
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //Función que incluye el máximo y el mínimo.
+}
+
+//Llamamos a la función para generar nuestro número
+var numberPlayed= getRandomIntInclusive(1, 200);
+console.log(numberPlayed);
+
+var buttonInput =document.querySelector('.button-input');
+
+function compare (){
+  var numIntroducido = document.getElementById('number-input').value;
+  var numIntroducido = parseInt (numIntroducido);
+  var tracks = document.getElementById('result');
+  if (numIntroducido===numberPlayed){
+    tracks.innerHTML='Has acertado';
+} else if (numIntroducido < numberPlayed){
+  tracks.innerHTML='El número que has introducido es menor que el número secreto';
+} else if (numIntroducido > numberPlayed){
+  tracks.innerHTML='El número que has introducido es mayor que el número secreto';
+}
+}
+
+
+buttonInput.addEventListener('click', compare);
+
+/*
+var contador = {inicial:0,
+								actual:0,
+								minimo:0,
+								maximo:100,
+								counting:function() {
+									if(contador.actual < contador.maximo){
+										contador.actual++;
+									}
+	document.getElementById('counter').innerHTML= contador.actual;
+
+								}
+
+};
+contador.counting();*/
