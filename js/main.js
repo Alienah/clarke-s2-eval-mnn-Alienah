54190a05ef66757db1af012a30f4d98e251c41ce
Alienah <aidalbla@gmail.com>
2017-12-05 20:55:09 +0100

    Add function to show tracks section

--- js/main.js
+++ js/main.js
@@ -27,11 +27,17 @@ function compare (){
   tracks.innerHTML='El número que has introducido es menor que el número secreto';
 } else if (numIntroducido > numberPlayed){
   tracks.innerHTML='El número que has introducido es mayor que el número secreto';
-} counting ++;
+} else {
+	tracks.innerHTML='Error. Tienes que introducir un número entre el 1 y el 200';
+}
+counting ++;
 	triesNumber.innerHTML= counting;
 }
 buttonInput.addEventListener('click', compare);
 
-function acertar{
-if (numIntroducido===numberPlayed){
+//Para mostrar las pistas
+function mostrarPistas () {
+	var tracksSection = document.querySelector('.pistas');
+	tracksSection.classList.add('show');
 }
+buttonInput.addEventListener('click', mostrarPistas);
