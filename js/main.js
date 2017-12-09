e03362f672f6abe97d39687e292e4fad7f3f93a1
Alienah <aidalbla@gmail.com>
2017-12-09 21:15:09 +0100

    Transform datas saved in objects and set rules to reset

--- js/main.js
+++ js/main.js
@@ -1,57 +1,72 @@
 'use strict';
-//Generar un número aleatorio entre 1 y 200
-
+var numberPlayed;
+//Generar un número aleatorio entre 1 y 100
+newNumber();
 function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; //Función que incluye el máximo y el mínimo.
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1)) + min; //Función que incluye el máximo y el mínimo.
+}
+function newNumber(){
+	//Llamamos a la función para generar nuestro número entre 1 y 100
+	numberPlayed = getRandomIntInclusive(1, 100);
+	console.log(numberPlayed);
 }
 
-//Llamamos a la función para generar nuestro número
-var numberPlayed= getRandomIntInclusive(1, 100);
-console.log(numberPlayed);
-
-var buttonInput =document.querySelector('.button-input');
-var triesNumber =document.querySelector('.counting-number');
+var buttonInput = document.querySelector('.button-input');
+var triesNumber = document.querySelector('.counting-number');
 var counting = 0;
+var inputSave = document.querySelector('.input-name-form');
 
 function compare (){
-  var numIntroducido = document.getElementById('number-input').value;
-  var numIntroducido = parseInt (numIntroducido);
-  var tracks = document.getElementById('result');
-  if (numIntroducido===numberPlayed){
-    tracks.innerHTML='Has acertado';
+	var numIntroduced = document.getElementById('number-input').value;
+	var numIntroduced = parseInt (numIntroduced);
+	var tracks = document.getElementById('result');
+	if (numIntroduced===numberPlayed){
+		tracks.innerHTML='Has acertado';
 		//Y te sale la pantalla para guardar
-		var inputSave = document.querySelector('.input-name-form');
 		inputSave.classList.add('show');
 
-} else if (numIntroducido > 200 || numIntroducido < 0){
-	tracks.innerHTML='El número debe ser comprendido entre 1 y 100';
-} else if (numIntroducido < numberPlayed){
-  tracks.innerHTML='El número que has introducido es menor que el número secreto';
-} else if (numIntroducido > numberPlayed){
-  tracks.innerHTML='El número que has introducido es mayor que el número secreto';
-} else {
-	tracks.innerHTML='Error. Tienes que introducir un número entre el 1 y el 100';
-}
-counting ++;
+	} else if (numIntroduced > 100 || numIntroduced < 0){
+		tracks.innerHTML='El número debe ser comprendido entre 1 y 100';
+	} else if (numIntroduced < numberPlayed){
+		tracks.innerHTML='El número que has introducido es menor que el número secreto';
+	} else if (numIntroduced > numberPlayed){
+		tracks.innerHTML='El número que has introducido es mayor que el número secreto';
+	} else {
+		tracks.innerHTML='Error. Tienes que introducir un número entre el 1 y el 100';
+	}
+	counting ++;
 	triesNumber.innerHTML= counting;
 }
+
 buttonInput.addEventListener('click', compare);
 
 //Para mostrar las pistas la primera vez
-function mostrarPistas () {
-	var tracksSection = document.querySelector('.pistas');
+var tracksSection = document.querySelector('.pistas');
+function showTracks() {
 	tracksSection.classList.add('show');
 }
-buttonInput.addEventListener('click', mostrarPistas);
+buttonInput.addEventListener('click', showTracks);
 
 //Para guardar en el histórico
 function savePunctuation() {
-	var itemData = '<li class="item-data">' + document.getElementById("name-input").value + ' - ' + counting + ' intentos.</li>'
-
+	var player = {
+								name: document.getElementById("name-input").value,
+								tries: counting,
+							};
+	var itemData = '<li class="item-data">' + player.name + ' - ' + player.tries + ' intentos.</li>'
 	var placeForData = document.getElementById('lista');
-  placeForData.insertAdjacentHTML('beforeend', itemData);
+	placeForData.insertAdjacentHTML('beforeend', itemData);
+
+	inputSave.classList.remove('show');
+	tracksSection.classList.remove('show');
+	newNumber();
+	counting = 0;
+	triesNumber.innerHTML= counting;
+	document.getElementById('number-input').value = '';
+	document.getElementById("name-input").value = '';
 }
+
 var buttonName = document.querySelector('.name-button');
 buttonName.addEventListener('click', savePunctuation);
